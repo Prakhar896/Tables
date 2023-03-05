8be50f3e579226cdc69096fe5f4367564fd2e85f
Prakhar Trivedi <53103894+Prakhar896@users.noreply.github.com>
2023-03-05 20:23:34 +0800

    Initial Commit

---
+++ Tables/TablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TablesApp.swift
+//  Tables
+//
+//  Created by Prakhar Trivedi on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct TablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
